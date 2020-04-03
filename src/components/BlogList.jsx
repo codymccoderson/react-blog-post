75a01cba9473521e0593b6b7caa896a3d1e92d94
Peter Jones <peteflux@gmail.com>
2020-04-03 13:18:13 -0400

    built BlogList and BlogPost components and about to test

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -1,3 +1,44 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+class BlogList extends Component {
+    state = {
+        allOfThePosts: []
+    }
+
+    async componentDidMount () {
+        const allOfThePosts = await this.loadData();
+        this.setState({
+            allOfThePosts
+        });
+    }
+
+    loadData = async () => {
+        const url = 'http://localhost:3000/v1/all';
+        const response = await fetch(url);
+        const data = response.json();
+        return data;
+    }
+
+    render () {
+        const { allOfThePosts } = this.state;
+
+        return (
+            <div>
+                <h2>Apocalpyse Now Blog</h2>
+                <ul>
+                    {allOfThePosts.map(post => {
+                        return(
+                        <li key={`posts/${post.id}`}>
+                            <Link to={`posts/${post.id}`}>{post.title}
+                            </Link>
+                        </li>
+                    );
+                    })}
+                </ul>
+            </div>
+        )
+    }
+}
 
 export default BlogList;
--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -1,3 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+class BlogPost extends Component {
+    state = {
+        thisOnePost: []
+    }
+
+    async componentDidMount () {
+        const thisOnePost = await this.loadData();
+        this.setState({
+            thisOnePost
+        });
+    }
+
+    loadData = async () => {
+        const PostId = this.props.match.params.p_id;
+        const url = `http://localhost:3000/v1/posts/:${PostId}?`
+        const response = await fetch(url);
+        data = response.json();
+        return data;
+    }
+
+    render () {
+        const { thisOnePost } = this.state;
+        return (
+            <div>
+                <h2>{thisOnePost.title}</h2>
+                <p>{thisOnePost.content}</p>
+            </div>
+        )
+    }
+}
 
 export default BlogPost;
